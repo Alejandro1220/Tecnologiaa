ae088351485e57fe9b800eb0157994ea62aff1b6
Alejandro1220 <70906172+Alejandro1220@users.noreply.github.com>
2021-01-24 18:23:30 -0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,12 @@
+ function ocultar() {
+ 	document.getElementById('mos').style.display="block";
+ }
+ 	        	function ocul() {
+ 	document.getElementById('ol').style.display="block";
+ }
+ 	        		    function oco() {
+ 	document.getElementById('la').style.display="block";
+ }
+              		        function ocu() {
+ 	document.getElementById('km').style.display="block";
+ }
